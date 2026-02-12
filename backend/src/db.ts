debf65a81d27571a1773d5f66b7d0b6e7cf7e077
Anurag <205334398+monarch-404@users.noreply.github.com>
2026-02-12 23:00:19 +0530

    Version 0.2

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,6 +1,7 @@
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
+//Load environment variables
 dotenv.config();
 
 const pool = new Pool({
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -13,13 +13,13 @@ router.get('/content/:id', getContent);
 
 // THis is for checking what I have sent on the server when I did not have a database but rather a dummy store.Can be removed later
 // --- ADD THIS DEBUG ROUTE ---
-router.get('/debug', async (req, res) => {
-  const allData = await Store.getAll();
-  res.json({
-    count: allData.length,
-    data: allData
-  });
-});
+// router.get('/debug', async (req, res) => {
+//   const allData = await Store.getAll();
+//   res.json({
+//     count: allData.length,
+//     data: allData
+//   });
+// });
 
 
 // CRITICAL: You must export default here for the import to work
